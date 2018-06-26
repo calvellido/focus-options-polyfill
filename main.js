77870794311e27b5dd2aa6a68c91a16568d8be49
Juan Valencia <juanvalencia@calvellido.es>
2018-06-26 17:49:52 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+// focus - focusOptions - preventScroll polyfill
+var supportsPreventScrollOption = false;
+try {
+  var focusElem = createElement('div');
+  focusElem.addEventListener('focus', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }, true);
+  // document.documentElement.focus(
+  focusElem.focus(
+    Object.defineProperty({}, 'preventScroll', { get: function () {
+      supportsPreventScrollOption = true;
+    }})
+  );
+} catch(e) {}
+
+if (HTMLElement.prototype.nativeFocus === undefined && !supportsPreventScrollOption) {
+
+  HTMLElement.prototype.nativeFocus = HTMLElement.prototype.focus;
+
+  var patchedFocus = function (args) {
+    var actualPosition = window.scrollY;
+    this.nativeFocus();
+    if (args.preventScroll) {
+      // Hijacking the event loop order, since the focus() will trigger
+      // internally an scroll that goes to the event loop
+      setTimeout(function() {
+        window.scroll(window.scrollX, actualPosition);
+      }, 0);
+    }
+  }
+
+  HTMLElement.prototype.focus = patchedFocus;
+
+}
