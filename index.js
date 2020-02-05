0290df926800b2b1279057d274c9d3a7b86954df
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2020-02-05 16:44:30 +0100

    Check if we are in a browser or not

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 // focus - focusOptions - preventScroll polyfill
-var supportsPreventScrollOption = false;
-try {
-  var focusElem = document.createElement('div');
-  focusElem.addEventListener('focus', function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-  }, true);
-  focusElem.focus(
-    Object.defineProperty({}, 'preventScroll', { get: function () {
-      supportsPreventScrollOption = true;
-    }})
-  );
-} catch(e) {}
-
-if (HTMLElement.prototype.nativeFocus === undefined && !supportsPreventScrollOption) {
-
-  HTMLElement.prototype.nativeFocus = HTMLElement.prototype.focus;
-
-  var patchedFocus = function (args) {
-    var actualPosition = window.scrollY || window.pageYOffset;
-    this.nativeFocus();
-    if (args && args.preventScroll) {
-      // Hijacking the event loop order, since the focus() will trigger
-      // internally an scroll that goes to the event loop
-      setTimeout(function() {
-        window.scroll((window.scrollX || window.pageXOffset), actualPosition);
-      }, 0);
-    }
+(function() {
+  if (
+    typeof window === "undefined" ||
+    typeof document === "undefined" ||
+    typeof HTMLElement === "undefined"
+  ) {
+    return;
   }
 
-  HTMLElement.prototype.focus = patchedFocus;
+  var supportsPreventScrollOption = false;
+  try {
+    var focusElem = document.createElement("div");
+    focusElem.addEventListener(
+      "focus",
+      function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+      },
+      true
+    );
+    focusElem.focus(
+      Object.defineProperty({}, "preventScroll", {
+        get: function() {
+          supportsPreventScrollOption = true;
+        }
+      })
+    );
+  } catch (e) {}
 
-}
+  if (
+    HTMLElement.prototype.nativeFocus === undefined &&
+    !supportsPreventScrollOption
+  ) {
+    HTMLElement.prototype.nativeFocus = HTMLElement.prototype.focus;
+
+    var patchedFocus = function(args) {
+      var actualPosition = window.scrollY || window.pageYOffset;
+      this.nativeFocus();
+      if (args && args.preventScroll) {
+        // Hijacking the event loop order, since the focus() will trigger
+        // internally an scroll that goes to the event loop
+        setTimeout(function() {
+          window.scroll(window.scrollX || window.pageXOffset, actualPosition);
+        }, 0);
+      }
+    };
+
+    HTMLElement.prototype.focus = patchedFocus;
+  }
+})();
